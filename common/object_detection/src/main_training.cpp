2b208f52337c8af55424021fe50e06c6c22bddf8
edgarriba <edgar.riba@gmail.com>
2014-06-22 01:10:42 +0200

    Training basics

--- common/object_detection/src/main_training.cpp
+++ common/object_detection/src/main_training.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
 
 
 int main(int argc, char *argv[])
@@ -48,4 +49,73 @@ int main(int argc, char *argv[])
 
   return 0;
 
+}
+
+void loadTrainingImages(const std::string &path, std::vector<cv::Mat> &list_images)
+{
+
+	cv::FileStorage storage(path, cv::FileStorage::READ + + cv::FileStorage::MEMORY);
+
+	cv::Mat list_names_mat;
+	std::vector<std::string> list_names;
+
+	storage["list_names"] >> list_names_mat;
+	storage.release();
+
+	list_names_mat.copyTo(list_images);
+
+	for (int i = 0; i < list_images.size(); ++i)
+	{
+		cv::Mat img = cv::imread(list_names[i], cv::IMREAD_COLOR);
+		list_images.push_back(img);
+	}
+
+}
+
+void makeTrainingMatrix(const std::vector<cv::Mat> &list_images, cv::Mat &training_mat)
+{
+	// Set training image
+	int num_files = 5;
+	int img_area = 4*3;
+	training_mat = cv::Mat(num_files,img_area,CV_32FC1);
+
+
+	for (int i = 0; i < list_images.size(); ++i)
+	{
+		const cv::Mat & img = list_images[i];
+
+		// TODO: scaling, rotation, gradients, etc ...
+		//cv::Mat img_scaled;
+		//cv::Mat img_rotated;
+		//cv::Mat img_gradients;
+
+		// Convert image to vector
+		cv::Mat img_vec = img.reshape(1, img.cols*img.cols);
+
+
+		// Add image to training matrix
+		training_mat.push_back(img_vec);
+
+	}
+
+}
+
+void trainSVM(cv::Ptr<CvSVM> svm, cv::Mat &training_mat, cv::Mat &labels)
+{
+	CvSVMParams params;
+	params.svm_type = CvSVM::C_SVC;
+	params.kernel_type = CvSVM::POLY;
+	params.gamma = 3;
+
+	svm->train(training_mat, labels, cv::Mat(), cv::Mat(), params); // training
+}
+
+void saveSVM(const cv::Ptr<CvSVM> svm, const std::string &write_path)
+{
+	svm->save(write_path.c_str()); // saving
+}
+
+void loadSVM(const cv::Ptr<CvSVM> svm, const std::string &read_path)
+{
+	svm->load(read_path.c_str()); // loading
 }
